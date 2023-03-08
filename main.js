ce20dc501f220eaa96907c512934361218560f83
Dominic Vonk <dominic.vonk@pon.com>
2023-03-08 10:53:51 +0100

    Bump improved katex

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import os from 'node:os';
 import path from 'node:path';
 import axios from 'axios';
 import { ConfluenceClient } from 'confluence.js';
-import { convert } from 'convert-svg-to-png';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import FastGlob from 'fast-glob';
 import { toHtml } from 'hast-util-to-html';
 import { fromMarkdown } from 'mdast-util-from-markdown';
@@ -12,10 +12,21 @@ import { mathFromMarkdown } from 'mdast-util-math';
 import { toHast } from 'mdast-util-to-hast';
 import { gfm } from 'micromark-extension-gfm';
 import { math } from 'micromark-extension-math';
+import { initialize, svg2png } from 'svg2png-wasm';
 import texsvg from 'texsvg';
 import { getInput } from '@actions/core';
 
+await initialize(
+  await readFile('./node_modules/svg2png-wasm/svg2png_wasm_bg.wasm'),
+);
+
 dotenv.config()
+const tempdir = process.env.NODE_ENV === 'dev' ? os.tmpdir() : '';
+
+if (tempdir) {
+  await mkdir(tempdir + '/.cache/md', {recursive: true});
+}
+
 var config = {
   username: getInput('auth_username') || process.env.auth_username,
   password: getInput('auth_api_token') || process.env.auth_api_token,
@@ -88,7 +99,7 @@ for (const file of files) {
         responseType: 'arraybuffer'
       })
       const buffer = await response.data
-      await writeFile(path.join('/.cache/md', image), toBuffer(buffer))
+      await writeFile(path.join(tempdir + '/.cache/md', image), toBuffer(buffer))
       assets.push(image)
       return {
         type: 'image',
@@ -98,10 +109,11 @@ for (const file of files) {
     if (node.type === 'math' || node.type === 'inlineMath') {
       const svg = await texsvg(node.value);
       const image = 'img-' + assets.length + '.png'
-      const png = await convert(svg, {
-        scale: 2
+      const png = await svg2png(svg, {
+        scale: 2,
+        backgroundColor: '#fff'
       })
-      await writeFile(path.join('/.cache/md', image), png)
+      await writeFile(path.join( tempdir + '/.cache/md', image), png)
       assets.push(image)
       return {
         type: 'image',
@@ -119,18 +131,27 @@ for (const file of files) {
     closeSelfClosing: true,
   })
 
-  html = html.replace(/<img src="(.*?)" \/>/g, '<ac:image><ri:attachment ri:filename="$1" /></ac:image>')
 
   const title = file.replace('/index.md', '').replace('.md', '').split('/').reverse()[0]
-  console.log(config)
+
 
   const content = await confluence.content.getContent({
     spaceKey: config.space,
     title,
   })
 
+  let version = 1
+if (content.results.length > 0) {
+  let id = content.results[0].id
+  const getContentById = await confluence.content.getContentById({
+    id
+  })
+  version = getContentById.version.number + 1
+}
+  html = html.replace(/<img src="(.*?)" \/>/g, '<ac:image><ri:attachment ri:filename="$1" /></ac:image>')
+
+
   let id = null;
-  let version = 1;
   const parent = file.replace('/index', '').split('/').reverse()?.[1]
   let parentId = configFile?.parent_id
   if (parent) {
@@ -160,14 +181,60 @@ for (const file of files) {
         number: version,
       },
     }, r))
-    id = newPage.id
+    const content = await confluence.content.getContent({
+      spaceKey: config.space,
+      title,
+    })
+
+
+    id = content.results[0].id
+    if (assets.length) {
+      console.log(version)
+      await confluence.contentAttachments.createOrUpdateAttachments({
+        space: {
+          key: config.space
+        },
+        attachments: await asyncMap(assets, async (asset) => {
+          console.log(asset)
+          return {
+          file: await readFile(path.join(tempdir + '/.cache/md', asset)),
+          filename: asset,
+          comment: 'Uploaded by exact-gateway',
+          minorEdit: true,
+
+        }}),
+        id,
+        version: {
+          number: version,
+        },
+      })
+    }
   } else {
     id = content.results[0].id
+
+    if (assets.length) {
+      console.log(version)
+      await confluence.contentAttachments.createOrUpdateAttachments({
+        space: {
+          key: config.space
+        },
+        attachments: await asyncMap(assets, async (asset) => {
+          console.log(asset)
+          return {
+          file: await readFile(path.join(tempdir + '/.cache/md', asset)),
+          filename: asset,
+          comment: 'Uploaded by exact-gateway',
+          minorEdit: true,
+
+        }}),
+        id,
+        version: {
+          number: version,
+        },
+      })
+    }
+  }
     console.log(`Update content: ${file}`)
-    const getContentById = await confluence.content.getContentById({
-      id
-    })
-    version = getContentById.version.number + 1
     await confluence.content.updateContent({
       space: {
         key: config.space
@@ -187,22 +254,3 @@ for (const file of files) {
       ancestors: parentId ? [{ id: parentId }] : [],
     })
   }
-  if (assets.length) {
-
-    await confluence.contentAttachments.createOrUpdateAttachments({
-      space: {
-        key: config.space
-      },
-      attachments: await asyncMap(assets, async (asset) => ({
-        file: await readFile(path.join('/.cache/md', asset)),
-        filename: asset,
-        comment: 'Uploaded by exact-gateway',
-        minorEdit: true,
-      })),
-      id,
-      version: {
-        number: version,
-      },
-    })
-  }
-}
